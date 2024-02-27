d08efffc5f43ff4e0af4e2e82e98aea414b2c6df
kkrishna09 <krishnarajput00009@gmail.com>
2024-02-28 02:20:40 +0530

    remove cart

--- scipts/amazon.js
+++ scipts/amazon.js
@@ -1,3 +1,6 @@
+import { addToCart , cart } from "../data/cart.js";
+import { products } from "../data/products.js";
+import { formatCurrency } from "../utils/money.js";
 let productsHTML='';
 products.forEach((product)=>{
     productsHTML +=`<div class="product-container">
@@ -19,7 +22,7 @@ products.forEach((product)=>{
     </div>
 
     <div class="product-price">
-      ${(product.priceCents /100).toFixed(2)}
+      $${formatCurrency(product.priceCents)}
     </div>
 
     <div class="product-quantity-container">
@@ -50,24 +53,26 @@ products.forEach((product)=>{
   </div>`
 })
 
+
+export function updateCartQuantity(){
+  let cartQuantity=0
+  cart.forEach((cartItem)=>{
+    cartQuantity +=cartItem.quantity;
+  })
+  document.querySelector(".js-cart-quantity").innerHTML=cartQuantity
+  }
+
+
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
-    button.addEventListener("click",()=>{
-        const productId=button.dataset.productId;
-        let matchingItem;
-        cart.forEach((item)=>{
-            if (productId==item.productId){
-                matchingItem=item
-            }
-        })
-        if (matchingItem){
-            matchingItem.quantity+=1
-        }else(
-            cart.push({
-                productId,
-                quantity:1
-            })
-        )
-        console.log(cart)
-    })
+  button.addEventListener("click",()=>{
+    const productId=button.dataset.productId;
+    addToCart(productId)
+    updateCartQuantity()
+    console.log(cart) 
+  })
 })
+
+
